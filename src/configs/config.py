9cf585f4e4d76ea0b38c54207616de875b5487d0
lwkys <Hasibuanfirman1404@gmail.com>
2026-06-07 03:13:06 +0700

    add vqa config

---
+++ src/configs/config.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import torch 
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+DATASET_DIR = PROJECT_ROOT / "dataset"
+IMAGE_DIR = DATASET_DIR / "images"
+TRAIN_CSV = DATASET_DIR / "data_train.csv"
+EVAL_CSV = DATASET_DIR / "data_eval.csv"
+FULL_CSV = DATASET_DIR / "data.csv"
+
+TRAIN_IMAGES_LIST = DATASET_DIR / "train_images_list.txt"
+TEST_IMAGES_LIST = DATASET_DIR / "test_images_list.txt"
+
+ALL_QA_PAIRS = DATASET_DIR / "all_qa_pairs.txt"
+ANSWER_SPACE = DATASET_DIR / "answer_space.txt"
+
+IMAGE_SIZE = 224
+SEED = 42
+BATCH_SIZE = 32
+EPOCHS = 10
+LEARNING_RATE = 1e-4
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
