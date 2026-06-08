748579e213fc15375ffbdf6b4d78e4767dfad625
lwkys <Hasibuanfirman1404@gmail.com>
2026-06-08 20:59:31 +0700

    add training and validation loops

--- src/engine/trainer.py
+++ src/engine/trainer.py
@@ -1,54 +1,55 @@
 import torch
+from src.metrics.vqa_metrics import AverageMeter, top_k_accuracy, accuracy
 
-def train_one_epoch(model, train_loader, criterion, optimizer, device):    
+def train_one_epoch(model, train_loader, criterion, optimizer, device):
     model.train()
-    
-    total_loss = 0.0
-    total_correct = 0
-    total_samples = 0
+
+    loss_meter = AverageMeter()
+    acc_meter = AverageMeter()
 
     for batch in train_loader:
-        images = batch['image'].to(device)
-        questions = batch['question'].to(device)
-        answers = batch['answer'].to(device)
+        images = batch["image"].to(device)
+        questions = batch["question"].to(device)
+        answers = batch["answer"].to(device)
 
         optimizer.zero_grad()
 
         logits = model(images, questions)
         loss = criterion(logits, answers)
+
         loss.backward()
         optimizer.step()
 
         batch_size = images.size(0)
-        total_loss += loss.item() * batch_size
-        total_samples += batch_size
-        total_correct += (logits.argmax(1) == answers).sum().item()
+        loss_meter.update(loss.item(), batch_size)
+        acc_meter.update(accuracy(logits, answers), batch_size)
 
-    avg_loss = total_loss / total_samples
-    accuracy = total_correct / total_samples
-    return avg_loss, accuracy
+    avg_loss = loss_meter.avg
+    acc = acc_meter.avg
+    return avg_loss, acc
 
 @torch.no_grad()
-def evaluate(model, val_loader, criterion, device):
+def validate_one_epoch(model, val_loader, criterion, device):
     model.eval()
 
-    total_loss = 0.0
-    total_correct = 0
-    total_samples = 0
+    loss_meter = AverageMeter()
+    acc_meter = AverageMeter()
+    top5_meter = AverageMeter()
 
     for batch in val_loader:
-        images = batch['image'].to(device)
-        questions = batch['question'].to(device)
-        answers = batch['answer'].to(device)
+        images = batch["image"].to(device)
+        questions = batch["question"].to(device)
+        answers = batch["answer"].to(device)
 
         logits = model(images, questions)
         loss = criterion(logits, answers)
 
         batch_size = images.size(0)
-        total_loss += loss.item() * batch_size
-        total_samples += batch_size
-        total_correct += (logits.argmax(1) == answers).sum().item()
+        loss_meter.update(loss.item(), batch_size)
+        acc_meter.update(accuracy(logits, answers), batch_size)
+        top5_meter.update(top_k_accuracy(logits, answers, k=5), batch_size)
 
-    avg_loss = total_loss / total_samples
-    accuracy = total_correct / total_samples
-    return avg_loss, accuracy
+    avg_loss = loss_meter.avg
+    acc = acc_meter.avg
+    top5_acc = top5_meter.avg
+    return avg_loss, acc, top5_acc
